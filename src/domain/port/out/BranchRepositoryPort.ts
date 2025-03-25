daf82e559cc535faf5e5f2a9ae678bb37fd738fc
Jonathan Urrego <jadougdev@gmail.com>
2025-03-25 08:12:44 -0500

    domain

--- src/domain/port/out/BranchRepositoryPort.ts
+++ src/domain/port/out/BranchRepositoryPort.ts
@@ -1,7 +1,7 @@
 import { Branch } from "src/domain/entity/Branch";
 
-export interface BranchServicePort {
-    create(branch: Branch): Promise<Branch>;
+export interface BranchRepositoryPort {
+    save(branch: Branch): Promise<Branch>;
     getById(id: number): Promise<Branch>;
     getAll(): Promise<Branch[]>;
     update(id: number, branch:Branch): Promise<Branch>;
---
+++ src/domain/service/BranchDomainService.ts
@@ -0,0 +1,31 @@
+import { Inject, Injectable } from "@nestjs/common";
+import { Branch } from "../entity/Branch";
+import { BranchServicePort } from "../port/in/BranchServicePort";
+import { BranchRepositoryPort } from "../port/out/BranchRepositoryPort";
+
+@Injectable()
+export class BranchDomainServce implements BranchServicePort {
+    constructor( @Inject("repositoryPort") 
+        private readonly repository: BranchRepositoryPort
+    ){}
+    
+    async create(branch: Branch): Promise<Branch> {
+        return await this.repository.save(branch);
+    }
+    
+    async getById(id: number): Promise<Branch> {
+        return await this.repository.getById(id);
+    }
+    
+    async getAll(): Promise<Branch[]> {
+        return await this.repository.getAll();
+    }
+    
+    async update(id: number, branch: Branch): Promise<Branch> {
+        return await this.repository.update(id, branch);
+    }
+    
+    async delete(id: number){
+     await this.repository.delete(id);
+    }
+}
